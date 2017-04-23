37e096393888be98ea45953a86839039592df715
Marcelo Seidel Jr <jackiedevpe@outlook.com.br>
2017-04-23 18:24:48 -0300

    Delete checkapi_top.js

--- src/js/checkapi_top.js
+++
@@ -1,5 +0,0 @@
-// Youtube
-var youtubetag = document.createElement('script');
-youtubetag.src = "https://www.youtube.com/iframe_api";
-var youtubefirstScriptTag = document.getElementsByTagName('script')[0];
-youtubefirstScriptTag.parentNode.insertBefore(youtubetag, youtubefirstScriptTag);
